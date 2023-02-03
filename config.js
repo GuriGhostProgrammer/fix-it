ae600f896dd5ef4768e191babb5e99d42eb945dd
GuriGhostProgrammer <85344279+GuriGhostProgrammer@users.noreply.github.com>
2023-02-03 19:32:16 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+   apiKey: "AIzaSyAapxnFHu1EFhU_Pgo77NHPU_ZJYYL-ziE",
+  authDomain: "fix-it-1be11.firebaseapp.com",
+  projectId: "fix-it-1be11",
+  storageBucket: "fix-it-1be11.appspot.com",
+  messagingSenderId: "1038251533045",
+  appId: "1:1038251533045:web:fd4601509cdf09938fdbbc"
+};
